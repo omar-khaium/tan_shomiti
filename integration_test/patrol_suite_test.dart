5ab152cff6f14f52b430f69df2968893652570a0
Omar Khaium Chowdhury <chowdhury.csm@gmail.com>
2026-02-03 18:22:43 +0600

    TS-102 (stage6): e2e tests

--- integration_test/patrol_suite_test.dart
+++ integration_test/patrol_suite_test.dart
@@ -3,6 +3,7 @@ import 'patrol_smoke_test.dart' as patrol_smoke_test;
 import 'ts_101_setup_wizard_e2e_test.dart' as ts_101_setup_wizard_e2e_test;
 import 'ts_002_components_gallery_e2e_test.dart' as ts_002_components_gallery_e2e_test;
 import 'ts_003_audit_ledger_e2e_test.dart' as ts_003_audit_ledger_e2e_test;
+import 'ts_102_governance_e2e_test.dart' as ts_102_governance_e2e_test;
 
 void main() {
   ts_101_setup_wizard_e2e_test.main();
@@ -10,4 +11,5 @@ void main() {
   patrol_e2e_test.main();
   ts_002_components_gallery_e2e_test.main();
   ts_003_audit_ledger_e2e_test.main();
+  ts_102_governance_e2e_test.main();
 }
